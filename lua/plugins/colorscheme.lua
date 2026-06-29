e48ad63f0559eefb11d22113388ea1dd7f9b6c21
Stefan Sedlarevic <raldesedlar@gmail.com>
2026-06-29 19:14:06 +0200

    changed colorscheme to modus

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,4 +1,28 @@
 return {
+  "miikanissi/modus-themes.nvim",
+  priority = 1000,
+  config = function()
+    vim.cmd("colorscheme modus")
+  end
+}
+
+--[[return {
+  "ficcdaf/ashen.nvim",
+  -- optional but recommended,
+  -- pin to the latest stable release:
+  tag = "*",
+  lazy = false,
+  priority = 1000,
+  -- configuration is optional!
+  opts = {
+    -- your settings here
+  },
+  config = function()
+    vim.cmd("colorscheme ashen")
+  end
+}--]]
+
+--[[return {
   "rebelot/kanagawa.nvim",
   lazy = false,
   priority = 1000,
@@ -6,4 +30,4 @@ return {
     -- wave / lotus / dragon
     vim.cmd("colorscheme kanagawa-wave")
   end,
-}
+}--]]
